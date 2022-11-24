8c5ff1529aa1f8d8f4fca9abf87329e6899d628d
sobhadityachak <118336312+sobhadityachak@users.noreply.github.com>
2022-11-24 17:56:21 +0530

    commit1

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+import {AiOutlineDelete,AiOutlineCheck} from 'react-icons/ai';
+
+
+function App() {
+  const [isCompleteScreen,setIsCompleteScreen] = useState(false);
+  const [allTodos,setTodos] = useState([]);
+  const [newTitle,setNewTitle] = useState('');
+  const [completedTodos,setCompletedTodos] = useState([]);
+
+  const handleAddTodo =() =>{
+    let newTodoItem={
+      title:newTitle,
+    };
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push (newTodoItem);
+    setTodos(updatedTodoArr);
+    localStorage.setItem('todolist',JSON.stringify(updatedTodoArr));
+  };
+
+  const handleDeleteTodo = (index) =>{
+    let reducedTodo = [...allTodos];
+    reducedTodo.splice(index);
+    localStorage.setItem('todolist' , JSON.stringify(reducedTodo));
+    setTodos(reducedTodo);
+  };
+
+  const handleComplete =(index) =>{
+    let now= new Date();
+    let dd = now.getDate();
+    let mm = now.getMonth() + 1;
+    let yyyy = now.getFullYear();
+    let h = now.getHours();
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    let completedOn = dd + '-' + mm + '-' + yyyy + ' at '  + h + ':' + m + ':' + s;
+
+    let filteredItem = {
+      ...allTodos[index],
+      completedOn: completedOn,
+    };
+
+    let updatedCompletedArr = [...completedTodos];
+    updatedCompletedArr.push (filteredItem);
+    setCompletedTodos (updatedCompletedArr);
+    handleDeleteTodo(index);
+    localStorage.setItem('completedTodos', JSON.stringfy(updatedCompletedArr));
+
+  };
+
+  const handleDeleteCompletedTodo = (index)=>{
+    let reducedTodo = [...completedTodos];
+    reducedTodo.splice(index);
+    localStorage.setItem('completedTodos' , JSON.stringify(reducedTodo));
+    setCompletedTodos(reducedTodo);
+  };
+
+  useEffect(()=>{
+    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
+    let savedCompletedTodo = JSON.parse(localStorage.getItem('completedTodos'));
+    if(savedTodo){
+      setTodos(savedTodo);
+    }
+    if(savedCompletedTodo){
+      setCompletedTodos(savedCompletedTodo);
+    }
+  },[])
+
+  return (
+    <div className="App">
+      <h1>TODO Web App</h1>
+
+      <div className='todo-wrapper'> 
+      <div className='todo-input'>
+        <div className='todo-input-item'>
+          <label>Task Name:</label>
+          <input type='text' value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder='Enter Task'/>
+        </div>
+        <div className='todo-input-item'>
+          <button type='button' onClick={handleAddTodo} className='primaryBtn'>Add Task</button>
+        </div>
+      </div>
+
+      <div className='btn-area'>
+        <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>Pending Tasks</button>
+        <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed Tasks</button>
+      </div>
+
+      <div className='todo-list'>
+
+      {isCompleteScreen===false && allTodos.map((item,index)=>{
+        return(
+          <div className='todo-list-item' key={index}>
+            <div>
+        <h3>{item.title}</h3>
+        </div>
+        <div>
+        <AiOutlineDelete className='icon' onClick={()=>handleDeleteTodo(index)} title="Delete?" />
+        <AiOutlineCheck className='check-icon' onClick={()=>handleComplete(index)} title='Complete?' />
+        </div>
+      </div>
+        );
+      })}
+
+        {isCompleteScreen===true && completedTodos.map((item,index)=>{
+        return(
+          <div className='todo-list-item' key={index}>
+            <div>
+        <h3>{item.title}</h3>
+        <p>Completed on: {item.completedOn}</p>
+        </div>
+        <div>
+        <AiOutlineDelete className='icon' onClick={()=>handleDeleteCompletedTodo(index)} title='Delete?' />
+        
+        </div>
+      </div>
+        );
+      })}
+      
+      </div>
+
+      </div>
+    </div>
+    
+   
+  );
+}
+
+export default App;
+  
